23803811e509b2463b2aaea512f58734cfade608
EugeneIvanov1 <6296785@stud.kai.edu.ua>
2025-04-02 00:26:04 +0300

    Add files via upload

--- Computer-Store-Management/Structure.cpp
+++ Computer-Store-Management/Structure.cpp
@@ -1,29 +1,26 @@
 #include "Header.cpp"
+#include <array>
+#include <string>
 
-//Construct node
-struct node
-{
-    int receipt_number, x;
+// Node structure definition
+struct Node {
+    int receiptNumber{};
+    std::string customerName;
+    std::string date;
+    std::array<int, 10> quantity{};
+    std::array<int, 10> menu2{};
+    std::array<int, 10> amount{};
+    int total{};
+    
+    static constexpr std::array<const char*, 10> type = {"8GB", "4GB", "4GB", "8GB", "8GB", "8GB", "4GB", "8GB", "4GB", "8GB"};
+    static constexpr std::array<const char*, 10> specifications = {"Intel Core i5(10th Gen)", "Intel Core i3 (10th Gen)", "Intel Core i5(8th Gen)", "Intel Core i5 (8th Gen)", "macOS Catalina", "Intel Core i5(10th Gen)", "Intel Core i3(5th Gen)", "Intel Core i3(7th Gen)", "Intel Core i5(6th Gen)", "Intel Core i7(10th Gen)"};
+    static constexpr std::array<const char*, 10> computerName = {"Dell Inspiron 15 5590", "Asus X412FA-EK361T", "Lenovo Ideapad 330", "Huawei Mach-W19B", "Apple MacBook Air", "HP 15s-du1034tu", "Acer Aspire ES1-571", "HP 15-DA0070TX", "Lenovo Ideapad 300", "HP Pavilion 15-ac028TX"};
+    static constexpr std::array<int, 10> computerPrices = {66490, 40000, 44000, 77899, 118990, 53999, 25655, 49279, 43000, 64000};
+    
+    Node* next = nullptr;
+};
 
-    string customerName, date;
-
-    int quantity[10], menu2[10];
-
-    int amount[10], total;
-
-    string type[10] = {"8GB", "4GB", "4GB", "8GB", "8GB", "8GB", "4GB", "8GB", "4GB", "8GB"};
-
-    string specifications[10] = {"Intel Core i5(10th Gen)", "Intel Core i3 (10th Gen)", "Intel Core i5(8th Gen)", "Intel Core i5 (8th Gen)", "macOS Catalina", "Intel Core i5(10th Gen)", "Intel Core i3(5th Gen)", "Intel Core i3(7th Gen)", "Intel Core i5(6th Gen)", "Intel Core i7(10th Gen)"};
-
-    string computerName[10] = {"Dell Inspiron 15 5590", "Asus X412FA-EK361T", "Lenovo Ideapad 330", "Huawei Mach-W19B", "Apple MacBook Air", "HP 15s-du1034tu", "Acer Aspire ES1-571", "HP 15-DA0070TX ", "Lenovo Ideapad 300", "HP Pavilion 15-ac028TX"};
-
-    int computer[10] = {66490, 40000, 44000, 77899, 118990, 53999, 25655, 49279, 43000, 64000};
-
-    // node *prev;
-    node *next;
-
-} *q, *temp; // Pointer declaration
-
-node *start_ptr = NULL;
-node *head = NULL;
-node *last = NULL;
+// Pointer declarations
+Node* startPtr = nullptr;
+Node* head = nullptr;
+Node* last = nullptr;
--- Computer-Store-Management/TakeOrder.cpp
+++ Computer-Store-Management/TakeOrder.cpp
@@ -1,125 +1,63 @@
-// Function to take order
-// Time Complexity - O(n)
-void computerType ::take_order()
-{
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class Node {
+public:
+    int receipt_number;
+    string customerName;
+    string date;
+    int x;
+    vector<int> menu2;
+    vector<int> quantity;
+    vector<double> amount;
+    vector<string> computerName;
+    vector<double> computer;
+    shared_ptr<Node> next;
+};
+
+class ComputerType {
+public:
+    void take_order();
+
+private:
+    shared_ptr<Node> start_ptr = nullptr;
+    void display_menu() const;
+};
+
+void ComputerType::display_menu() const {
+    cout << "********************************************************************************************************************\n";
+    cout << " ID \t   COMPUTER TYPE\t COMPUTER SPECS\t	 COMPUTER NAME\t	COMPUTER PRICE (RS.)\n";
+    cout << "********************************************************************************************************************\n";
+    vector<tuple<int, string, string, string, double>> computers = {
+        {1, "8GB", "Intel Core i5(10th Gen)", "Dell Inspiron 15 5590", 66490.00},
+        {2, "4GB", "Intel Core i3(10th Gen)", "Asus X412FA-EK361T", 40000.00},
+        {3, "4GB", "Intel Core i5(8th Gen)", "Lenovo Ideapad 330", 44000.00},
+        {4, "8GB", "Intel Core i5 (8th Gen)", "Huawei Mach-W19B", 77899.00},
+        {5, "8GB", "macOS Catalina", "Apple MacBook Air", 118990.00},
+        {6, "8GB", "Intel Core i5(10th Gen)", "HP 15s-du1034tu", 53999.00},
+        {7, "4GB", "Intel Core i3(5th Gen)", "Acer Aspire ES1-571", 25655.00},
+        {8, "4GB", "Intel Core i3(7th Gen)", "HP 15-DA0070TX", 49279.00},
+        {9, "4GB", "Intel Core i5(6th Gen)", "Lenovo Ideapad 300", 43000.00},
+        {10, "8GB", "Intel Core i7(5th Gen)", "HP Pavilion 15-ac028TX", 64000.00}
+    };
+
+    for (const auto& comp : computers) {
+        cout << get<0>(comp) << "\t" << get<1>(comp) << "\t\t" << get<2>(comp) << "\t" << get<3>(comp) << "\t" << get<4>(comp) << "\n";
+    }
+    cout << "********************************************************************************************************************\n";
+}
+
+void ComputerType::take_order() {
     system("cls");
-    int i;
-    int choice, quantity, price, None;
-    string str1 = "********************************************************************************************************************\n";
-    string str2 = "+-------+-----------------+---------------------------------+------------------------------+-----------------------+\n";
+    cout << "-----------------\nAdd Order Details\n-----------------\n\n";
 
-    cout << "-----------------";
-    cout << "\nAdd Order Details\n";
-    cout << "-----------------\n\n";
+    auto temp = make_shared<Node>();
+    display_menu();
 
-    node *temp;
-    temp = new node;
-
-    cout << str1;
-    cout << " ID "
-         << "\t   COMPUTER TYPE"
-         << "\t COMPUTER SPECS"
-         << "   \t\t COMPUTER NAME"
-         << "    \t\tCOMPUTER PRICE (RS.)" << endl;
-    cout << str1;
-    cout << "0001"
-         << "\t"
-         << "\t8GB"
-         << "\t\t"
-         << " Intel Core i5(10th Gen)"
-         << "\t"
-         << " Dell Inspiron 15 5590  "
-         << "  	66490.00" << endl;
-    cout << str2;
-    cout << "0002"
-         << "\t"
-         << "\t4GB"
-         << "\t\t"
-         << " Intel Core i3(10th Gen)"
-         << "\t"
-         << " Asus X412FA-EK361T     "
-         << "	40000.00" << endl;
-    cout << str2;
-    cout << "0003"
-         << "\t"
-         << "\t4GB"
-         << "\t\t"
-         << " Intel Core i5(8th Gen) "
-         << "\t"
-         << " Lenovo Ideapad 330     "
-         << "	44000.00" << endl;
-    cout << str2;
-    cout << "0004"
-         << "\t"
-         << "\t8GB"
-         << "\t\t"
-         << " Intel Core i5 (8th Gen)  "
-         << "\t"
-         << " Huawei Mach-W19B"
-         << "       \t77899.00" << endl;
-    cout << str2;
-    cout << "0005"
-         << "\t"
-         << "\t8GB"
-         << "\t\t"
-         << " macOS Catalina         "
-         << "\t"
-         << " Apple MacBook Air      "
-         << "	118990.00" << endl;
-    cout << str2;
-    cout << "0006"
-         << "\t"
-         << "\t8GB"
-         << "\t\t"
-         << " Intel Core i5(10th Gen)"
-         << "\t"
-         << " HP 15s-du1034tu        "
-         << " 	53999.00" << endl;
-    cout << str2;
-    cout << "0007"
-         << "\t"
-         << "\t4GB"
-         << "\t\t"
-         << " Intel Core i3(5th Gen) "
-         << "\t"
-         << " Acer Aspire ES1-571    "
-         << "	25655.00" << endl;
-    cout << str2;
-    cout << "0008"
-         << "\t"
-         << "\t4GB"
-         << "\t\t"
-         << " Intel Core i3(7th Gen) "
-         << "\t"
-         << " HP 15-DA0070TX           "
-         << "	49279.00" << endl;
-    cout << str2;
-    cout << "0009"
-         << "\t"
-         << "\t4GB"
-         << "\t\t"
-         << " Intel Core i5(6th Gen) "
-         << "\t"
-         << " Lenovo Ideapad 300     "
-         << "        43000.00" << endl;
-    cout << str2;
-    cout << "0010"
-         << "\t"
-         << "\t8GB"
-         << "\t\t"
-         << " Intel Core i7(5th Gen) "
-         << "\t"
-         << " HP Pavilion 15-ac028TX "
-         << "	64000.00" << endl;
-    cout << str1;
-    cout << " " << endl;
-
-    temp = new node;
-
-    string str = "-------------------------------------------\n";
-
-    cout << str;
-
+    cout << "-------------------------------------------\n";
     cout << "Type Order No: ";
     cin >> temp->receipt_number;
 
@@ -129,70 +67,51 @@ void computerType ::take_order()
     cout << "Enter Date: ";
     cin >> temp->date;
 
-    cout << str;
-
-    cout << "\nHow many different laptops would you like to order?";
-    cout << "\n(Maximum order is 10 for each transaction): ";
+    cout << "-------------------------------------------\n";
+    cout << "\nHow many different laptops would you like to order? (Maximum 10 per transaction): ";
     cin >> temp->x;
 
-    cout << endl;
-
-    if (temp->x <= 0)
-    {
-         cout << "\nInvalid order!!!\n";
-         cout << str;
-         system("pause");
-    }
-
-    else if (temp->x > 10)
-    {
-        cout << "\nYour order exceeds the maximum amount of order!!!\n"; 
-        cout << str;
+    if (temp->x <= 0 || temp->x > 10) {
+        cout << "\nInvalid order! Maximum order is 10.\n";
         system("pause");
+        return;
     }
 
-    else
-    {
-         cout << str;
-         
-         cout << "\t    Enter Computer ID\n";
+    temp->menu2.resize(temp->x);
+    temp->quantity.resize(temp->x);
+    temp->amount.resize(temp->x);
+    
+    for (int i = 0; i < temp->x; i++) {
+        cout << "-------------------------------------------\n";
+        cout << "Please Enter Computer ID: ";
+        cin >> temp->menu2[i];
 
-         for (i = 0; i < temp->x; i++)
-         {
-              cout << str;
-
-              cout << "Please Enter Your Selection: ";
-              cin >> temp->menu2[i];
-
-              cout << "\nLaptop Name: " << temp->computerName[temp->menu2[i] - 1] << endl;
-
-              cout << "\nHow many laptops do you want?: ";
-              cin >> temp->quantity[i];
-
-              temp->amount[i] = temp->quantity[i] * temp->computer[temp->menu2[i] - 1];
-              cout << "\nThe amount You need to pay is: Rs. " << temp->amount[i] << "/-" << endl;
-
-              cout << str << endl;
-
-              system("PAUSE");
+        if (temp->menu2[i] < 1 || temp->menu2[i] > 10) {
+            cout << "Invalid Computer ID. Try again.\n";
+            --i;
+            continue;
         }
 
-        cout << "\n===========================================================================" << endl;
-        cout << "                     Order Taken Successfully..." << endl;
-        cout << "===========================================================================" << endl;
-        cout << "                Go to Receipt Menu to Pay The Bill" << endl;
-        cout << "===========================================================================\n" << endl;
+        cout << "\nHow many laptops do you want?: ";
+        cin >> temp->quantity[i];
 
+        vector<double> prices = {66490.00, 40000.00, 44000.00, 77899.00, 118990.00, 53999.00, 25655.00, 49279.00, 43000.00, 64000.00};
+        temp->amount[i] = temp->quantity[i] * prices[temp->menu2[i] - 1];
+        cout << "\nThe amount you need to pay is: Rs. " << temp->amount[i] << "/-\n";
+
+        cout << "-------------------------------------------\n";
         system("PAUSE");
-
-        temp->next = NULL;
-
-        if (start_ptr != NULL)
-            temp->next = start_ptr;
-
-        start_ptr = temp;
-
-        system("cls");
     }
 
-} // End function take_order
+    cout << "===========================================================================" << endl;
+    cout << "                     Order Taken Successfully..." << endl;
+    cout << "===========================================================================" << endl;
+    cout << "                Go to Receipt Menu to Pay The Bill" << endl;
+    cout << "===========================================================================\n" << endl;
+
+    system("PAUSE");
+
+    temp->next = start_ptr;
+    start_ptr = temp;
+    system("cls");
+}
